80d5112c8c8a4a5ce4d48d607f74943e704d045c
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2024-11-16 13:24:48 +0300

    init launcher script

---
+++ src/launcher.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+macOS_version=$(sw_vers -productVersion)
+arch=$(uname -m)
+
+case "$macOS_version" in
+15*)
+    macOS_version=sequoia
+    ;;
+14*)
+    macOS_version=sonoma
+    ;;
+13*)
+    macOS_version=ventura
+    ;;
+12*)
+    macOS_version=monterey
+    ;;
+11*)
+    macOS_version=bigsur
+    ;;
+10.15*)
+    macOS_version=catalina
+    ;;
+10.14*)
+    macOS_version=mojave
+    ;;
+10.13*)
+    macOS_version=high_sierra
+    ;;
+esac
+
+if [ "$arch" = "arm64" ]; then
+    macOS_version=arm64_$macOS_version
+    link="https://github.com/hyperpuncher/mac-soft-rs/releases/download/latest/mac-soft-rs-arm64"
+else
+    link="https://github.com/hyperpuncher/mac-soft-rs/releases/download/latest/mac-soft-rs"
+fi
+
+temp_file=$(mktemp)
+chmod +x "$temp_file"
+curl -sL "$link" -o "$temp_file"
+
+"$temp_file" "$macOS_version"
